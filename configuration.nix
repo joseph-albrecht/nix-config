7e113c97a2ca82585699852a020aef5d5cb323b7
Joey Albrecht <joey@joseph-albrecht.me>
2020-04-04 14:54:19 -0400

    change hostname to geode

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device  = "/dev/sda";
 
-  networking.hostName = "mymachine"; 
+  networking.hostName = "geode"; 
   networking.networkmanager.enable = true;
 
   services.xserver = {
